ad86a3230cf3a1da1052b77d8d588e6f0b9e1926
darryl1975 <darryl1975@hotmail.com>
2024-10-03 14:00:44 +0800

    implemented truck interfaces

--- vehicle/Truck.java
+++ vehicle/Truck.java
@@ -17,6 +17,30 @@ public class Truck extends Vehicle {
         this.cargoCapacity = cargoCapacity;
     }
 
+    @Override
+    public void start() {
+        System.out.println("Truck started...");
+    }
 
+    @Override
+    public void stop() {
+        this.setSpeed(0);
+        System.out.println("Truck stopped...");
+    }
+
+    @Override
+    public void accelerate(Integer increment) {
+        this.setSpeed(this.getSpeed() + increment + 2);
+    }
+
+    // @Override
+    // public void brake(Integer decrement) {
+    //     this.speed = this.speed - decrement;
+    // }
+
+    @Override
+    public void honk() {
+        System.out.println("Honk...honk...honk...");
+    }
     
 }
