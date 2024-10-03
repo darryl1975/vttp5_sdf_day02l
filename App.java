2a5a9969fdea0931180bae3a31dad5e947a995a4
darryl1975 <darryl1975@hotmail.com>
2024-10-03 11:52:13 +0800

    updated App.java

--- App.java
+++ App.java
@@ -1,6 +1,11 @@
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import object.Computer;
+import object.Desktop;
+import object.Laptop;
 
 public class App {
     
@@ -20,5 +25,31 @@ public class App {
         computer2.startup();
         computer2.displaySpec();
         computer2.shutdown();
+
+        Laptop laptop01 = new Laptop();
+        laptop01.setCpu("10GHz 128 core");
+        laptop01.setModel("Asus 12000");
+        laptop01.setRam(64);
+        laptop01.setYearMade(2024);
+        laptop01.setBattery("11.1V 3S LIPO");
+        laptop01.setWeight(3);
+        laptop01.setOperatingSystem("Windows 11 Professional");
+        laptop01.setScreenSize("15.4 inch OLED");
+        laptop01.startup();
+        laptop01.displaySpec();
+        laptop01.shutdown();
+
+        Desktop desktop01 = new Desktop("4GHz 8 core", "Alibaba", 16, 2024, "Ubuntu 20.4 LTS", 5, true);
+        desktop01.startup();
+        desktop01.displaySpec();
+        desktop01.shutdown();
+
+        List<Computer> machines = new ArrayList<>();
+        machines.add(desktop01);
+        machines.add(computer);
+        machines.add(computer2);
+        machines.add(laptop01);
+
+
     }
 }
